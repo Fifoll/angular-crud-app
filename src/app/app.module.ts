125caf87ed27218e6bd4e01cc4be870ad7a4970d
fifoll <kakolfilip@wp.pl>
2023-06-23 13:13:24 +0200

    initial commit - html structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharactersListComponent } from './characters-list/characters-list.component';
+import { CharacterFormComponent } from './character-form/character-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersListComponent,
+    CharacterFormComponent
   ],
   imports: [
     BrowserModule
